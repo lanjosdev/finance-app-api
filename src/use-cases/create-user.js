2dc3f9dcdd02fc8c9efc1d19804627f9110e634f
Lucas dos Anjos <lucas.anjospro@gmail.com>
2026-05-22 02:42:10 -0300

    feat: add CreateUserUseCase
    - Added bcrypt dependency for password hashing.
    - Added uuid dependency for generating unique user IDs.
    - Implemented CreateUserUseCase to create a user with hashed password and unique ID.
    - User creation logic includes a placeholder for email existence check.

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,28 @@
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
+import { PostgresCreateUserRepository } from '../respositories/postgres/create-user.js';
+
+export class CreateUserUseCase {
+    async execute(createUserParams) {
+        // TODO: verificar se o email já existe
+
+        // gerar um ID único para o usuário
+        const userId = uuidv4();
+
+        // criptografar a senha
+        const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
+
+        // criar o objeto do usuário
+        const user = {
+            ...createUserParams,
+            id: userId,
+            password: hashedPassword,
+        };
+
+        // criar o usuário no banco de dados
+        const postgresCreateUserRepository = new PostgresCreateUserRepository();
+        const createdUser = await postgresCreateUserRepository.execute(user);
+
+        return createdUser;
+    }
+}
