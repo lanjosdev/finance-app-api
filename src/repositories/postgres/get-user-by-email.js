bf1f2bdccf0b8dd8b44742529eb3ba605d5b55f6
Lucas dos Anjos <lucas.anjospro@gmail.com>
2026-05-26 18:00:43 -0300

    feat: add GetUserByEmailRepository

--- src/repositories/postgres/get-user-by-email.js
+++ src/repositories/postgres/get-user-by-email.js
@@ -1,10 +1,10 @@
 import { PostgresHelper } from '../../db/postgres/helper.js';
 
-export class PostgresGetUserByIdRepository {
-    async execute(userId) {
+export class PostgresGetUserByEmailRepository {
+    async execute(email) {
         const results = await PostgresHelper.query(
-            'SELECT id, first_name, last_name, email FROM users WHERE id = $1',
-            [userId],
+            'SELECT * FROM users WHERE email = $1',
+            [email],
         );
         const user = results[0];
 
--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,16 +1,27 @@
 import { v4 as uuidv4 } from 'uuid';
 import bcrypt from 'bcrypt';
 import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js';
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js';
 
 export class CreateUserUseCase {
     async execute(createUserParams) {
-        // TODO: verificar se o email já existe
+        const { email: userEmail, password: userPassword } = createUserParams;
+
+        // Verificar se o email já existe
+        const postgresGetUserByEmailRepository =
+            new PostgresGetUserByEmailRepository();
+        const existingUser =
+            await postgresGetUserByEmailRepository.execute(userEmail);
+
+        if (existingUser) {
+            throw new Error('Email already exists');
+        }
 
         // gerar um ID único para o usuário
         const userId = uuidv4();
 
         // criptografar a senha
-        const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
+        const hashedPassword = await bcrypt.hash(userPassword, 10);
 
         // criar o objeto do usuário
         const user = {
